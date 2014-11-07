1e9bed03aa353edb9fa8b7be287fdb0aacc0507e
varunkumar3618 <Varun@dn0a2233d9.sunet>
2014-11-07 15:32:32 -0800

    Number and type of appointments in each presidency
    Some metric to measure the normalized difference between regular and
    establishment appointments. Have to still normalize for length of the
    presidency.

--- normalize.py
+++ normalize.py
@@ -1,22 +1,35 @@
 import csv
 print(__name__)
 if __name__ == "__main__":
-	f_obj1 = open("data/PostMastersPresidents.csv")
+	f_obj1 = open("data/PostMastersInRange.csv")
 	reader = csv.DictReader(f_obj1)
 
 	PM = []
 	for row in reader:
 		PM.append(row)
 	f_obj1.close()
-
-	f_obj2 = open("data/OfficesByState_Total.csv")
-	reader = csv.DictReader(f_obj2)
 	
-	TotalOffices = 0
+	NumOffices = [452,494,489,521,596,654,749,818,861,908,1022,1131,1218,1344,1364,1438,1448,1604,1684,1830,1907,2045,2106,2248,2349,2477,2572,2687,2730,2755,2766,2860,2968,3112,3188,3245]
 
-	for row in reader:
-		if row["State"] == "Texas":
-			totalOffices = row
-			break
-	f_obj2.close()
-	print Totaloffices
+	for pm in PM:
+		ind = int(pm["Year"]) - 1867
+		pm["Weight"] = NumOffices[ind]
+
+	Presidents = []
+	PresidentsEst = []
+	i = 0
+	while i < 36:
+		Presidents.append(0)
+		PresidentsEst.append(0)
+		i = i + 1
+
+	for pm in PM:
+		ind = int(pm["President"])
+		w = 1.0/float(int(pm["Weight"]))
+		if pm["NewApp"] == "0":
+			Presidents[ind] = Presidents[ind] + w
+		else:
+			PresidentsEst[ind] = PresidentsEst[ind] + w
+
+	print Presidents
+	print PresidentsEst
