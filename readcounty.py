d58f3c048f82a1b20d26c474800905ff4acd101b
varunkumar3618 <Varun@dn0a22381b.sunet>
2014-11-12 12:23:41 -0800

    Read counties with est/change ratios

--- readcounty.py
+++ readcounty.py
@@ -6,13 +6,12 @@ def nameyear(pm):
 
 print(__name__)
 if __name__== "__main__":
-	pm_path = "data/Texas_Postmasters.csv"
+	pm_path = "data/newPostmastersmulti.csv"
 	f_obj1 = open(pm_path, "rU")
 	pm_reader = csv.DictReader(f_obj1);
 
 	PM = []
 	Counties = []
-
 
 	for row in pm_reader:
 		PM.append(row)
@@ -24,32 +23,51 @@ if __name__== "__main__":
 			CountyNames.append(pm['County'])
 			CountyAll = {}
 			CountyAll['County'] = pm['County']
+			CountyAll['Est'] = '-'
 			CountyAll['Year'] = '-'
-			CountyAll['Num'] = 1
-			Counties.append(CountyAll)	
+			CountyAll['EstNum'] = 0
+			CountyAll['ChangeNum'] = 0
+			if pm['NewApp'] == 0:
+				CountyAll['ChangeNum'] = CountyAll['ChangeNum'] + 1
+			else:
+				CountyAll['EstNum'] = CountyAll['EstNum'] + 1
+			Counties.append(CountyAll)
 
-			County = {}
-			County['County'] = pm['County']
-			County['Year'] = pm['Year']
-			County['Num'] = 1
-			Counties.append(County)
+			CountyAll = {}
+			CountyAll['County'] = pm['County']
+			CountyAll['Year'] = pm['Year']
+			CountyAll['EstNum'] = 0
+                        CountyAll['ChangeNum'] = 0
+                        if pm['NewApp'] == 0:
+                                CountyAll['ChangeNum'] = CountyAll['ChangeNum'] + 1
+                        else:
+                                CountyAll['EstNum'] = CountyAll['EstNum'] + 1
+			Counties.append(CountyAll)
 
 		else:
 			i = 0
 			for county in Counties:
 				if county['County'] == pm['County']:
 					if county['Year'] == pm['Year'] or county['Year'] == '-':
-						county['Num'] = county['Num'] + 1
+						if pm['NewApp'] == 0:
+							county['ChangeNum'] = county['ChangeNum'] + 1
+						else:
+							county['EstNum'] = county['EstNum'] + 1
 						i = i + 1
 			if i < 2:
 				County = {}
                 		County['County'] = pm['County']
                 		County['Year'] = pm['Year']
-                		County['Num'] = 1
+				County['EstNum'] = 0
+				County['ChangeNum'] = 0
+				if pm['NewApp'] == 0:
+					County['ChangeNum'] = County['ChangeNum'] + 1
+				else:
+					County['EstNum'] = County['EstNum'] + 1 
                 		Counties.append(County)
 	
 	fieldnames = Counties[0].keys()		
-	new_path = 'data/Counties.csv'
+	new_path = 'data/Countiesnew.csv'
 	f_obj2 = open(new_path, 'wb')
 	writer = csv.DictWriter(f_obj2, delimiter=',',fieldnames=fieldnames)
 	f = dict(zip(fieldnames,fieldnames))
